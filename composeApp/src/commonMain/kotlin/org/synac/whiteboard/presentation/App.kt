896567a6385a0e8764c793a86fa5481f5c30a5c1
Mohammad Arif <danielharold21@gmail.com>
2024-10-19 21:13:11 +0530

    added color scheme

--- composeApp/src/commonMain/kotlin/org/synac/whiteboard/presentation/App.kt
+++ composeApp/src/commonMain/kotlin/org/synac/whiteboard/presentation/App.kt
@@ -2,21 +2,19 @@ package org.synac.whiteboard.presentation
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.synac.whiteboard.presentation.navigation.NavGraph
+import org.synac.whiteboard.presentation.theme.WhiteboardTheme
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme(
-        colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()
+    WhiteboardTheme(
+        darkTheme = isSystemInDarkTheme()
     ) {
         val navController = rememberNavController()
 
